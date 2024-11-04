97e09cf89448a9ff13a73ef4cf332a580ba8ac55
Adrian Caballero <92646371+Schlaufuchs17@users.noreply.github.com>
2024-11-04 22:52:18 +0100

    Delete dist directory

--- dist/client/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './style.css';
-
-ReactDOM.render(<App />, document.getElementById('app'));
